87129bfff81cb9f2683a27b182c4736813fa2559
tomiharu0317 <tomiharu1017@gmail.com>
2020-08-10 23:47:43 +0900

    made basic structure

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import render_template
+import os
+
+from myproject import app
 
 @app.route('/')
-def hello():
-    name = "Hello World"
-    return name
+def index():
+    return render_template('home.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
