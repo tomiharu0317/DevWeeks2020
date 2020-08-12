5752107e729ff6b628e465f4c8b9b0de43990d3e
tomiharu0317 <tomiharu1017@gmail.com>
2020-08-12 11:46:37 +0900

    add task folder

--- myproject/task/views.py
+++ myproject/task/views.py
@@ -8,6 +8,9 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from myproject import db, app
 from myproject.models import User
+# from myproject.task.forms import (FixedScheduleForm, OtherWorksForm,
+#                                   ReportForm, TestForm, TestPrepareForm,
+#                                   PresentationForm, OndemandClassForm)
 
 task_blueprint = Blueprint('task',
                            __name__,
@@ -19,8 +22,84 @@ def scheduletoday():
 
     return  redirect(url_for('auth.login')) if not current_user.is_authenticated else render_template('schedule_today.html')
 
+
+
 @task_blueprint.route('/alltasks')
 @login_required
 def alltasks():
 
     return  redirect(url_for('auth.login')) if not current_user.is_authenticated else render_template('all_tasks.html')
+
+
+
+
+@task_blueprint.route('/addtask')
+@login_required
+def addtask():
+
+    return  redirect(url_for('auth.login')) if not current_user.is_authenticated \
+            else render_template('add_task.html')
+
+
+
+
+@task_blueprint.route('/addtask/fixedschedule')
+@login_required
+def fixedschedule():
+
+    return  redirect(url_for('auth.login')) if not current_user.is_authenticated \
+            else render_template('fixedschedule.html')
+
+
+
+@task_blueprint.route('/addtask/otherworks')
+@login_required
+def otherworks():
+
+    return  redirect(url_for('auth.login')) if not current_user.is_authenticated \
+            else render_template('otherworks.html')
+
+
+
+@task_blueprint.route('/addtask/report')
+@login_required
+def report():
+
+    return  redirect(url_for('auth.login')) if not current_user.is_authenticated \
+            else render_template('report.html')
+
+
+
+@task_blueprint.route('/addtask/test')
+@login_required
+def test():
+
+    return  redirect(url_for('auth.login')) if not current_user.is_authenticated \
+            else render_template('test.html')
+
+
+
+@task_blueprint.route('/addtask/testprepare')
+@login_required
+def testprepare():
+
+    return  redirect(url_for('auth.login')) if not current_user.is_authenticated \
+            else render_template('testprepare.html')
+
+
+
+@task_blueprint.route('/addtask/presentation')
+@login_required
+def presentation():
+
+    return  redirect(url_for('auth.login')) if not current_user.is_authenticated \
+            else render_template('presentation.html')
+
+
+
+@task_blueprint.route('/addtask/ondemandclass')
+@login_required
+def ondemandclass():
+
+    return  redirect(url_for('auth.login')) if not current_user.is_authenticated \
+            else render_template('ondemandclass.html')
